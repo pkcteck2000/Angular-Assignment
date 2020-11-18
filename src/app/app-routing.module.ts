c81c1c293b2f4c3222bf6de933fe2ec9a2e98f47
premrobosoft <premnath@robosoftin.com>
2020-11-18 20:14:23 +0530

    1) Change password component is done with close icon 2) Type added to password input

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ChangePasswordComponent } from './components/change-password/change-password.component';
+import { HomeScreenComponent } from './components/home-screen/home-screen.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
-  { path: 'login', component: LoginComponent }
+  { path: '', component: HomeScreenComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'update-password', component: ChangePasswordComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,20 +9,26 @@ import { LoginComponent } from './components/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { ChangePasswordComponent } from './components/change-password/change-password.component';
+import { HomeScreenComponent } from './components/home-screen/home-screen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
     FooterComponent,
-    LoginComponent
+    LoginComponent,
+    ChangePasswordComponent,
+    HomeScreenComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
